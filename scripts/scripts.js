5c11de0c84b07efabb1aca3ba52dd441b20f7982
Marcus <2161524@slu.edu.ph>
2018-04-24 20:14:44 +0800

    Added calendar themes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,13 @@
 $(function () {
-  $('.calendar').fullCalendar({});
+  $('.calendar').fullCalendar({
+    header: {
+      left: 'prevYear,prev,today',
+      center: 'title',
+      right: 'today,next,nextYear'
+    },
+    titleFormat: 'MMMM YYYY',
+    selectable: true,
+    themeSystem: 'bootstrap4',
+    height: 'parent'
+  });
 });
