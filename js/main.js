7fb10c3bb918053db77496c1429ef93a4fc93190
Marcus <2161524@slu.edu.ph>
2018-05-04 23:53:08 +0800

    Fixed header position, Added title

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ $(function () {
   $('.fixed-action-btn').floatingActionButton();
 
   $('#search').on('click', function () {
-    $('#header').addClass('h-25').removeClass('fixed');
+    $('#header').addClass('h-25');
+    $('#events').addClass('top-25');
     $('#logo').hide(500);
   });
 });
