bb5cf877b1a1a64028f1fd0f1486d6f2f2f70036
Marcus <2161524@slu.edu.ph>
2018-05-05 19:16:51 +0800

    Modified layout, Added links

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
 $(function () {
   'use strict';
-
+  
   $('.fixed-action-btn').floatingActionButton();
-
+  $('.chips').chips();
+  $('.chips-autocomplete').chips({
+    autocompleteOptions: {
+      data: {
+        'Apple': null,
+        'Microsoft': null,
+        'Google': null
+      },
+      limit: Infinity,
+      minLength: 1
+    }
+  });
+  $('.chips-placeholder').chips({
+    placeholder: 'Enter a tag',
+    secondaryPlaceholder: '+Tag'
+  });
+  
   $('#search').on('click', function () {
     $('#header').addClass('h-25');
     $('#events').addClass('top-25');
